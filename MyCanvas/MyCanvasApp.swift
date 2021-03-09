5610593b581aefb0730c5ec8870803064cdbae44
Salome Brack <salome.brack@gmx.de>
2021-03-09 12:06:17 +0100

    Initial Commit

---
+++ MyCanvas/MyCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCanvasApp.swift
+//  MyCanvas
+//
+//  Created by Student on 09.03.21.
+//
+
+import SwiftUI
+
+@main
+struct MyCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
