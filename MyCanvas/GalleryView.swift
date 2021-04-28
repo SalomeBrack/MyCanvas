c7767e9cc64f269db2da1e2b5a4499a4ac6a6837
Salome Brack <salome.brack@gmx.de>
2021-04-29 00:58:21 +0200

    GalleryView mit Daten

--- MyCanvas/GalleryView.swift
+++ MyCanvas/GalleryView.swift
@@ -5,92 +5,52 @@
 //  Created by Student on 09.03.21.
 //
 
+
 import SwiftUI
-import CoreData
 
 struct GalleryView: View {
     @Environment(\.managedObjectContext) private var viewContext
-    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Drawing.timestamp, ascending: true)], animation: .default)
-    private var drawings: FetchedResults<Drawing>
-    
-    @State private var drawingName: String = "Untitled"
-    @State private var showAddDrawing: Bool = false
+    @FetchRequest(entity: Drawing.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Drawing.timestamp, ascending: true)], animation: .default) private var drawings: FetchedResults<Drawing>
     
     var body: some View {
         List {
             ForEach(drawings) { drawing in
-                NavigationLink(destination: CanvasView(drawingId: drawing.id!), label: {
-                    Text("\(drawing.name!) - \(drawing.timestamp!, formatter: dateFormatter)")
+                NavigationLink(destination: CanvasView(drawingId: drawing.id ?? UUID()), label: {
+                    Text("\(drawing.name ?? "Untitled") - \(drawing.timestamp ?? Date(), formatter: dateFormatter)")
                 })
             }
-            .onDelete(perform: deleteDrawings)
+            .onDelete(perform: deleteDrawing)
         }
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
                 EditButton()
             }
             ToolbarItem(placement: .navigationBarTrailing) {
-                Button(action: { showAddDrawing = true },
-                       label: { Label("Add Drawing", systemImage: "plus") })
+                Button(action: addDrawing, label: { Label("New Drawing", systemImage: "plus") })
             }
         }
-        .sheet(isPresented: $showAddDrawing, content: {
-            HStack() {
-                Text("Name: ")
-                TextField("Drawing Name", text: $drawingName, onCommit: { addDrawing() })
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }.padding()
-            
-            Button(action: {
-                showAddDrawing = false
-                addDrawing()
-            }, label: { Text("Create") })
-        })
     }
     
-    private func addDrawing() {
-        let newDrawing = Drawing(context: viewContext)
-        newDrawing.id = UUID()
-        newDrawing.name = drawingName
-        newDrawing.timestamp = Date()
-        newDrawing.data = Data()
-        
-        do { try viewContext.save() } catch {
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
+    func addDrawing() {
+        let drawing = Drawing(context: viewContext)
+        drawing.id = UUID()
+        drawing.name = "Drawing"
+        drawing.timestamp = Date()
+        drawing.data = Data()
+        PersistenceController.shared.save()
     }
     
-    
-    private func editDrawing(offsets: IndexSet) {
-        //
-        
-        do { try viewContext.save() } catch {
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-    }
-
-    private func deleteDrawings(offsets: IndexSet) {
-        offsets.map { drawings[$0] }.forEach(viewContext.delete)
-        
-        do { try viewContext.save() } catch {
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    func deleteDrawing(at offsets: IndexSet) {
+        for index in offsets {
+            let drawing = drawings[index]
+            PersistenceController.shared.delete(drawing)
         }
     }
 }
 
 private let dateFormatter: DateFormatter = {
     let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
+    formatter.dateStyle = .long
+    formatter.timeStyle = .none
     return formatter
 }()
-
-struct GalleryView_Previews: PreviewProvider {
-    static var previews: some View {
-        GalleryView()
-            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
-}
